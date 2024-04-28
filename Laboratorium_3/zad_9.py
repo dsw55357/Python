18892a277b83f77cc52a965cbda5fd349634ce19
dsw55357 <dsw55357@student.dsw.edu.pl>
2024-04-28 13:32:32 +0200

    zad_9 completed

---
+++ Laboratorium_3/zad_9.py
@@ -0,0 +1,56 @@
+"""
+
+9. Program, który symuluje działanie „LOTTO”
+
+"""
+import random
+
+def wylosuj_liczby():
+    
+    liczby = []
+    
+    while len(liczby) < 6:
+        nowa_liczba = random.randint(1, 49)
+        if nowa_liczba not in liczby:
+            liczby.append(nowa_liczba)
+
+    return liczby      
+
+def czy_jest_wygrana(numery_klienta, lotto):
+    print(f"Numery klienta: {numery_klienta}")
+    print(f"Numery LOTTO: {lotto}")     
+
+    trafione=0
+
+    for liczba in numery_klienta:
+         if liczba in lotto:
+              trafione +=1
+
+    match trafione:
+        case 6: 
+            print(f"Liczba trafień: {trafione}")
+        case 5: 
+            print(f"Liczba trafień: {trafione}")
+        case 4: 
+            print(f"Liczba trafień: {trafione}")
+        case 3: 
+            print(f"Liczba trafień: {trafione}")
+        case 2: 
+            print(f"Liczba trafień: {trafione}")
+        case 1: 
+            print(f"Liczba trafień: {trafione}")
+        case _: 
+            print(f"Brak trafień")
+
+
+
+def main():
+    
+    numery_gracza = wylosuj_liczby()
+    numery_LOTTO = wylosuj_liczby()
+
+    czy_jest_wygrana(numery_gracza, numery_LOTTO)
+    
+            
+if __name__ == "__main__":
+        main()
