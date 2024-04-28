92aaeda02d4fc96d5b728069eb9e5dc84d89578e
dsw55357 <dsw55357@student.dsw.edu.pl>
2024-04-28 12:00:59 +0200

    text corrected

--- Laboratorium_3/zad_8.py
+++ Laboratorium_3/zad_8.py
@@ -10,10 +10,9 @@ def max_liczba_niepodzielna():
         if i % 2 != 0 and i % 3 != 0 and i % 5 != 0 and i % 7 != 0:
             return i
 
-
 def main():
 
-    print(f"największą liczbę niepodzielną : {max_liczba_niepodzielna()}")
+    print(f"Największa liczba niepodzielna : {max_liczba_niepodzielna()}")
             
 if __name__ == "__main__":
         main()
