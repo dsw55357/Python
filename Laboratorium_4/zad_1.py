fe4d5cd84fb3e48d860a49fd72a3262d6a24c63b
dsw55357 <dsw55357@student.dsw.edu.pl>
2024-04-29 21:28:01 +0200

    zad_1 completed

---
+++ Laboratorium_4/zad_1.py
@@ -0,0 +1,16 @@
+"""
+
+Program przy użyciu pętli while który wyświetli na ekranie 100 razy napis : To jest efekt
+pętli while.
+
+"""
+
+def main():
+    
+    x = 0
+    while x < 100:
+          print(f"{x+1}. To jest efekt pętli while.")
+          x += 1
+            
+if __name__ == "__main__":
+        main()
