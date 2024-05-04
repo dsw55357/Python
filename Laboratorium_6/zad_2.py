36193da0b705150fc2a67c03cd1b0c5c271b6293
dsw55357 <dsw55357@student.dsw.edu.pl>
2024-05-04 22:00:16 +0200

    zad_2 completed

---
+++ Laboratorium_6/zad_2.py
@@ -0,0 +1,18 @@
+"""
+
+Program w którym zadeklarujesz w tablicy 10 dowolnych elementów liczbowych i program wypisze 1,4,7,9 element z tej tablicy.
+
+"""
+
+def main():
+    tablica = [1,2,3,4,5,6,7,8,9,10]
+
+    print(f"1. {tablica[0]}")
+    print(f"4. {tablica[3]}")
+    print(f"7. {tablica[6]}")
+    print(f"9. {tablica[8]}")
+
+if __name__ == "__main__":
+    
+    main()
+
