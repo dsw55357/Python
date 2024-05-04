efb5ca4c25c82fd32ee2507e173a97d654055f6e
dsw55357 <dsw55357@student.dsw.edu.pl>
2024-05-04 21:44:27 +0200

    zad_1 completed

---
+++ Laboratorium_6/zad_1.py
@@ -0,0 +1,18 @@
+"""
+
+Program w którym zadeklarujesz w tablicy 10 dowolnych elementów znakowych i program wypisze 1,4,7,9 element z tej tablicy.
+
+"""
+
+def main():
+    tablica = ['W', 'r', 'o', 'c', 'ł', 'a', 'w', 'D', 'S', 'W']
+
+    print(f"1. {tablica[0]}")
+    print(f"4. {tablica[3]}")
+    print(f"7. {tablica[6]}")
+    print(f"9. {tablica[8]}")
+
+if __name__ == "__main__":
+    
+    main()
+
