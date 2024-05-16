42abd515bd107351bfedba0b34b8dd7a04f4a139
dsw55357 <dsw55357@student.dsw.edu.pl>
2024-05-16 23:48:03 +0200

    zad_4 completed

---
+++ Laboratorium_10/zad_4.py
@@ -0,0 +1,20 @@
+"""
+
+
+"""
+
+przyklady = f"Przykłady wykorzystania operacji na napisach w pracy programisty: \ 
+            Przetwarzanie danych tekstowych z różnych źródeł, które mogą mieć różne wielkości liter.\
+            Formatowanie tekstu \
+            Korekta błędów ortograficznych w tekście \
+            Parsowanie plików tekstowych z różnym formatowaniem \
+            Szyfrowanie tekstu prostą metodą odwrócenia kolejności liter \
+            Analiza tekstu w celu określenia częstości występowania słów \
+            Wyszzukiwanie konkretnych fraz w tekście \
+            Weryfikacja danych wejściowych użytkownika (np. sprawdzanie, czy hasło zawiera tylko litery i cyfry). \
+            Identyfikacja typu pliku na podstawie jego nagłówka \
+            Weryfikacja poprawności adresu URL.
+            "
+            # Istnienie metod typu string ma ogromne znaczenie dla programistów, ponieważ ułatwiają one pracę z tekstem i automatyzują wiele zadań, które w innym przypadku wymagałyby ręcznego kodowania. 
+
+print(przyklady)
