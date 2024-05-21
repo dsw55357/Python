c767308233663cfaedeeec4e4801f0e5eb8eaaac
dsw55357 <dsw55357@student.dsw.edu.pl>
2024-05-21 22:36:21 +0200

    wersja podniesiona

--- projekt/asteroids.py
+++ projekt/asteroids.py
@@ -4,7 +4,7 @@ Projekt na zalicznie
 
 Asteroids – gra komputerowa stworzona przez firmę Atari, wydana została w roku 1979 na automaty do gry oraz konsolę Atari 2600. W roku 1981 została wydana na komputer Atari 800. Firma Atari wydała jeszcze oficjalne wersje na konsole Atari 5200 i Atari 7800 oraz na komputery Acorn BBC Micro i Atari ST. 
 
-Status: v.0.2
+Status: v.0.3
 
 """
 
