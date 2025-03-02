4b01a9f96c1d89eeea968f9bd8dece8f251f2442
Twoje Imię Nazwisko <twoj@email.com>
2025-03-02 23:08:39 +0100

    README.md added

--- projekt/asteroids.py
+++ projekt/asteroids.py
@@ -6,7 +6,7 @@ Projekt na zalicznie
 
 Gra komputerowa stworzona przez firmę Atari, wydana została w roku 1979 na automaty do gry oraz konsolę Atari 2600. W roku 1981 została wydana na komputer Atari 800. Firma Atari wydała jeszcze oficjalne wersje na konsole Atari 5200 i Atari 7800 oraz na komputery Acorn BBC Micro i Atari ST. 
 
-Status: v.0.3
+Status: v.0.4
 
 Inspired by Pixel Game Engine by Javidx9
 
